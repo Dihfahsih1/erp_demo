cee571095babb1f108c038f361f6be71e3fa71da
Dihfahsih1 <dihfahsihm@gmail.com>
2025-04-04 04:43:24 +0300

    add custom settings for site administration

--- erp_demo/settings.py
+++ erp_demo/settings.py
@@ -86,14 +86,14 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
  
-JAZZMIN_SETTINGS = {
-    "site_logo": "/img/Logo.png",
-    "login_logo": None,  # Also removes the logo on the login page
-    "site_title": "AutoZone Admin",
-    "site_header": "AutoZone ERP",
-    "site_brand": "AutoZone",
-    "site_logo_classes": "img-circle",
-    "welcome_sign": "Welcome to AutoZone ERP Admin",
+JAZZMIN_SETTINGS = { 
+    # ============ Branding ============
+    "site_title": "AutoZone Admin",  # Browser tab title
+    "site_header": "AutoZone ERP",  # Login/signup header
+    "site_brand": "AutoZone Pro",  # Sidebar text
+    "site_logo": "img/Logo.png",  # Sidebar logo
+    "login_logo": "img/Logo.png",  # Login page logo
+    "site_logo_classes": "img-fluid",  # Better than img-circle for rectangular logos
     "copyright": "AutoZone Ltd",
     "user_avatar": None,
     
@@ -160,9 +160,7 @@ JAZZMIN_SETTINGS = {
             "permissions": ["books.view_book"]
         }]
     },
-
-    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
-    # for the full list of 5.13.0 free icon classes
+ 
     "icons": {
         "auth": "fas fa-users-cog",
         "auth.user": "fas fa-user",
@@ -191,13 +189,7 @@ JAZZMIN_SETTINGS = {
 
     ###############
     # Change view #
-    ###############
-    # Render out the change view as a single form, or in tabs, current options are
-    # - single
-    # - horizontal_tabs (default)
-    # - vertical_tabs
-    # - collapsible
-    # - carousel
+    ############### 
     "changeform_format": "horizontal_tabs",
     # override change forms on a per modeladmin basis
     "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
@@ -211,7 +203,8 @@ JAZZMIN_SETTINGS = {
             "icon": "fas fa-file-invoice",
             "text": "You have {} pending estimates",
         }
-    }
+    },
+  
 }
 
 JAZZMIN_UI_TWEAKS = {
@@ -226,15 +219,7 @@ JAZZMIN_UI_TWEAKS = {
     "sidebar": "sidebar-dark-primary",
 }
 
-JAZZMIN_SETTINGS = {
-    "custom_links": {
-        "dashboard": [{
-            "name": "Sales Analytics",
-            "url": "/admin/sales-analytics/",
-            "icon": "fas fa-chart-line",
-        }]
-    }
-}
+ 
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
--- erp_demo/urls.py
+++ erp_demo/urls.py
@@ -7,5 +7,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('erp.urls'))
+    path('',include('erp.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),   
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

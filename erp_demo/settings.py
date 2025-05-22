9f21b2b85a23c7a49e675e434dad72a9defc4c44
Dihfahsih1 <dihfahsihm@gmail.com>
2025-05-22 15:27:23 +0300

    remove comments to make code readable

--- erp_demo/settings.py
+++ erp_demo/settings.py
@@ -111,57 +111,28 @@ JAZZMIN_SETTINGS = {
     "show_ui_builder": True,
     "changeform_format": "horizontal_tabs",
     
-    
-    ############
-    # Top Menu #
-    ############
-
-    # Links to put along the top menu
+     
     "topmenu_links": [
-
-        # Url that gets reversed (Permissions can be added)
+ 
         {"name": "Home",  "url": "admin:index", "permissions": ["auth.view_user"]},
-
-        # external url that opens in a new window (Permissions can be added)
+ 
         {"name": "Support", "url": "#", "new_window": True},
-
-        # model admin to link to (Permissions checked against model)
+ 
         {"model": "auth.User"},
-
-        # App with dropdown menu to all its models pages (Permissions checked against models)
+ 
         {"app": "books"},
     ],
-
-    #############
-    # User Menu #
-    #############
-
-    # Additional links to include in the user menu on the top right ("app" url type is not allowed)
+ 
     "usermenu_links": [
         {"name": "Support", "url": "#", "new_window": True},
         {"model": "auth.user"}
     ],
-
-    #############
-    # Side Menu #
-    #############
-
-    # Whether to display the side menu
-    "show_sidebar": True,
-
-    # Whether to aut expand the menu
-    "navigation_expanded": True,
-
-    # Hide these apps when generating side menu e.g (auth)
-    "hide_apps": [],
-
-    # Hide these models when generating side menu (e.g auth.user)
-    "hide_models": [],
-
-    # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
-    "order_with_respect_to": ["auth", "books", "books.author", "books.book"],
-
-    # Custom links to append to app groups, keyed on app name
+ 
+    "show_sidebar": True, 
+    "navigation_expanded": True, 
+    "hide_apps": [], 
+    "hide_models": [], 
+    "order_with_respect_to": ["auth", "books", "books.author", "books.book"], 
     "custom_links": {
         "books": [{
             "name": "Make Messages", 
@@ -175,35 +146,18 @@ JAZZMIN_SETTINGS = {
         "auth": "fas fa-users-cog",
         "auth.user": "fas fa-user",
         "auth.Group": "fas fa-users",
-    },
-    # Icons that are used when one is not manually specified
+    }, 
     "default_icon_parents": "fas fa-chevron-circle-right",
     "default_icon_children": "fas fa-circle",
-
-    #################
-    # Related Modal #
-    #################
-    # Use modals instead of popups
+ 
     "related_modal_active": False,
-
-    #############
-    # UI Tweaks #
-    #############
-    # Relative paths to custom CSS/JS scripts (must be present in static files)
+ 
     "custom_css": None,
-    "custom_js": None,
-    # Whether to link font from fonts.googleapis.com (use custom_css to supply font otherwise)
-    "use_google_fonts_cdn": True,
-    # Whether to show the UI customizer on the sidebar
-    "show_ui_builder": False,
-
-    ###############
-    # Change view #
-    ############### 
-    "changeform_format": "horizontal_tabs",
-    # override change forms on a per modeladmin basis
-    "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
-    # Add a language dropdown into the admin
+    "custom_js": None, 
+    "use_google_fonts_cdn": True, 
+    "show_ui_builder": False, 
+    "changeform_format": "horizontal_tabs", 
+    "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"}, 
     "language_chooser": True,
 
     "notifications": {
